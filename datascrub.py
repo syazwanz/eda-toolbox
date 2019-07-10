b022169c8aa2f6e2b1986c14ffd66ca265949392
msyazwan <41930926+msyazwan@users.noreply.github.com>
2019-07-10 15:07:24 +0800

    Update datascrub.py

--- datascrub.py
+++ datascrub.py
@@ -1,27 +1,33 @@
-def remove_col_white_space(df,col):
-    df[col] = df[col].str.lstrip()
+def remove_char(df, col, char):
+    df[col] = df[col].str.strip(char)
 
-def convert_cat2num(col, df):
-    num_encode = {col : {1:'YES', 0:'NO'},
-                  'col_2'  : {'WON':1, 'LOSE':0, 'DRAW':0}}  
+
+def cat2num(df, col):
+    num_encode = {col : {'YES':1, 'NO':0}}  
     df.replace(num_encode, inplace=True)
 
-def change_dtypes(col_int, col_float, df): 
-    df[col_int] = df[col_int].astype('int32')
-    df[col_float] = df[col_float].astype('float32')
 
-def check_missing_data(df):
+def change_int_float(df, col_name, integer = True):
+    if integer:
+        df[col_name] = df[col_name].astype('int32')
+    else:
+        df[col_name] = df[col_name].astype('float32')
+
+
+def missing_data(df):
     return df.isnull().sum().sort_values(ascending=False)
 
-def concat_col_str_condition(df):
-    # concat 2 columns with strings if the last 3 letters of the first column are 'pil'
-    mask = df['col_1'].str.endswith('pil', na=False)
-    col_new = df[mask]['col_1'] + df[mask]['col_2']
-    col_new.replace('pil', ' ', regex=True, inplace=True)  # replace the 'pil' with emtpy space
 
-def convert_str_datetime(df): 
-    df.insert(loc=2, column='timestamp', value=pd.to_datetime(df.transdate, format='%Y-%m-%d %H:%M:%S.%f'))
+def concat_col_condition(df, col1, col2, char, col_replace = False):
+    mask = df[col1].str.endswith(char, na=False)
+    col_new = df[mask][col1] + df[mask][col2]
+    col_new.replace(char, ' ', regex=True, inplace=True)
+    
+    if col_replace:
+        df[col1] = col_new
+    else:
+        df['col_new'] = col_new
 
-def drop_col(col_names_list, df): 
+
+def drop_col(df, col_names_list): 
     df.drop(col_names_list, axis=1, inplace=True)
-    return df
